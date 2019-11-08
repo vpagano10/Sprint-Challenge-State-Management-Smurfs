6954d35a2aa576cd7f70a11e75a92ce435c00e04
vpagano10 <vpagano10@gmail.com>
2019-11-08 13:54:21 -0500

    answers to self study questions

--- server.js
+++ server.js
@@ -18,6 +18,12 @@ let smurfs = [
     age: 200,
     height: '5cm',
     id: 0
+  },
+  {
+    name: 'Name',
+    age: 200,
+    height: '5cm',
+    id: 1
   }
 ];
 server.get('/smurfs', (req, res) => {
